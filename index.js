7be88379c0d973e99ca70a28c20e257948c33631
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2017-08-24 18:03:49 +0200

    Use Commander and Add Option to remove some pages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict'
 
 const co = require('co')
@@ -5,6 +6,7 @@ const fs = require('fs')
 const os = require('os')
 const inquirer = require('inquirer')
 const scissors = require('scissors')
+const program = require('commander')
 
 const checkPath = (path) => {
   if (path[0] === '.') {
@@ -16,49 +18,105 @@ const checkPath = (path) => {
   return path
 }
 
-co(function *() {
-  console.log('Guetting ready for PDF Concatenation...')
-  const input = yield inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Where are the files you need to concatenate ?',
-      name: 'paths'
-    },
-    {
-      type: 'number',
-      message: 'How many pages do you have to concatenate ?',
-      name: 'number'
-    }
-  ])
-
-  const filesToConcatenate = checkPath(input.paths)
-  let pdfBuffers
-  const numberOfPages = parseInt(input.number)
-
-  for (let i = 0; i < numberOfPages; i++) {
-    let tmpPath = filesToConcatenate + i + '.pdf'
-    if (fs.existsSync(tmpPath)) {
-      if (i === 0) {
-        pdfBuffers = scissors(tmpPath)
-      } else {
-        pdfBuffers = scissors.join(pdfBuffers, scissors(tmpPath))
+const concatenate = () => {
+  co(function *() {
+    console.log('Getting ready for PDF Concatenation...')
+    const input = yield inquirer.prompt([
+      {
+        type: 'input',
+        message: 'Where are the files you need to concatenate ?',
+        name: 'paths'
+      },
+      {
+        type: 'number',
+        message: 'How many pages do you have to concatenate ?',
+        name: 'number'
       }
-    } else {
-      throw new Error('pdf file at ' + tmpPath + ' does not exists')
+    ])
+
+    const filesToConcatenate = checkPath(input.paths)
+    let pdfBuffers
+    const numberOfPages = parseInt(input.number)
+
+    for (let i = 0; i < numberOfPages; i++) {
+      let tmpPath = filesToConcatenate + i + '.pdf'
+      if (fs.existsSync(tmpPath)) {
+        if (i === 0) {
+          pdfBuffers = scissors(tmpPath)
+        } else {
+          pdfBuffers = scissors.join(pdfBuffers, scissors(tmpPath))
+        }
+      } else {
+        throw new Error('PDF file at ' + tmpPath + ' does not exists')
+      }
     }
-  }
 
-  const output = filesToConcatenate.slice(0,-1) + '.pdf'
+    const output = filesToConcatenate + '.pdf'
 
-  pdfBuffers
-    .pdfStream()
-    .pipe(fs.createWriteStream(output))
-    .on('finish', () => {
-      console.log('The new pdf is ready at ' + output)
-    })
-    .on('error', (error) => {
-      throw new Error('An error occured while concatening the pdfs:', error)
-    })
-}).catch((error) => {
-  console.error(error)
-})
+    pdfBuffers
+      .pdfStream()
+      .pipe(fs.createWriteStream(output))
+      .on('finish', () => {
+        console.log('The new pdf is ready at ' + output)
+      })
+      .on('error', (error) => {
+        throw new Error('An error occured while concatening the PDFs:', error)
+      })
+  }).catch((error) => {
+    console.error(error)
+  })
+}
+
+const remove = () => {
+  co(function *() {
+    console.log('Getting ready to remove some pages of the PDF')
+    const userInput = yield inquirer.prompt([
+      {
+        type: 'input',
+        message: 'Where are the files you need to remove page from ?',
+        name: 'input'
+      },
+      {
+        type: 'input',
+        message: 'What are the path you want the new PDF to be save at ?',
+        name: 'output',
+        default: 'override'
+      },
+      {
+        type: 'number',
+        message: 'Which page you want to keep (start) ?',
+        name: 'start',
+        default: 0
+      },
+      {
+        type: 'number',
+        message: 'Which page you want to keep (finish) ?',
+        name: 'finish',
+        default: 0
+      }
+    ])
+
+    const input = checkPath(userInput.input)
+    const output = userInput.output === 'override' ? input : checkPath(userInput.output)
+
+    scissors(input)
+      .range(userInput.start, userInput.finish)
+      .pdfStream()
+      .pipe(fs.createWriteStream(output))
+      .on('finish', () => {
+        console.log('The PDF is ready at' + output)
+      })
+      .on('error',(error) => {
+        throw new Error('An error occured while removing pages in the PDF:', error);
+      })
+  }).catch((error) => {
+    console.error(error)
+  })
+}
+
+program
+  .version('1.0.1')
+  .usage('[options]')
+  .option('-c, --concatenate', 'Concatenate PDF', concatenate)
+  .option('-r, --remove', 'Remove Pages', remove)
+  .parse(process.argv)
